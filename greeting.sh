12e36db3233e67a03bbfa0bd3335f165aa1afd1b
ilosrim <ilosrim@yahoo.com>
2022-02-05 16:20:22 +0500

    bash scripts

---
+++ greeting.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# upper case > {$name^l}
+# lower case > {$name^h}
+echo "Ismingizni kiriting: "
+read name
+echo {$name^^}
---
+++ upperLower.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "enter a char"
+read c
+
+if [[ $c == [A-Z] ]];
+then
+    echo "upper"
+elif [[ $c == [a-z] ]];
+then
+    echo "lower"
+else 
+    echo "Digit or special symbols!"
+fi
